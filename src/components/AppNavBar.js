f4ea93138fd6bb82458cc3373058c41a716dd67a
Daniel Flores D <daniel.flor3s.d@gmail.com>
2023-02-10 13:01:16 -0700

    Bootswatch integration

---
+++ src/components/AppNavBar.js
@@ -0,0 +1,40 @@
+// import 'bootswatch/dist/flatly/bootstrap.min.css';
+import {
+  Navbar,
+  Nav,
+  Container
+} from "react-bootstrap";
+import React from "react";
+
+class AppNavbar extends React.Component {
+  render() {
+    return (
+      <Navbar expand="sm" bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link active href="#">
+                Home
+              </Nav.Link>
+              <Nav.Link href="#">Features</Nav.Link>
+              <Nav.Link href="#">Pricing</Nav.Link>
+              <Nav.Link href="#">About</Nav.Link>
+              <Nav.Link href="#" dropdown="true">
+                Dropdown
+              </Nav.Link>
+              <Nav.Link href="#">Action</Nav.Link>
+              <Nav.Link href="#">Another action</Nav.Link>
+              <Nav.Link href="#">Something else here</Nav.Link>
+              <Nav.Link href="#" disabled>
+                Separated link
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+export default AppNavbar;
