d36abcc02f37f180fa7c97374cbba893f91e10e3
Daniel Flores D <daniel.flor3s.d@gmail.com>
2023-02-11 11:14:48 -0700

    2x3 grid

--- src/components/AppCard.js
+++ src/components/AppCard.js
@@ -3,37 +3,27 @@ import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 // import Button from 'react-bootstrap/Button';
 // import { CardBody, CardTitle, CardText } from 'react-bootstrap/Card';
+import { Github } from "react-bootstrap-icons";
 
-import logo from "./website.PNG";
-
-class AppCard extends React.Component {
-  render() {
-    return (
-      <Card>
-        {/* style={{ width: '250px', height: '150px' }}> */}
-        <Card.Img variant="top" src={logo} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <a href="#" variant="link">
-            Go somewhere
+function AppCard({ title, description, url, github, tech, image }) {
+  return (
+    <Card>
+      <Card.Img src={image} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <a href={url} variant="link">
+          {url}
+        </a>
+      </Card.Body>
+      <Card.Footer>
+        <Badge bg="secondary">
+          <a href={github} data-tooltip="danielfloresd">
+            <Github />
           </a>
-        </Card.Body>
-        <Card.Footer>
-          <Badge bg="secondary">GitHub</Badge>
-          <a
-            href="https://github.com/danielfloresd/"
-            target="_blank"
-            data-tooltip="danielfloresd"
-          >
-            <i class="github large icon"></i>
-          </a>
-        </Card.Footer>
-      </Card>
-    );
-  }
+        </Badge>
+      </Card.Footer>
+    </Card>
+  );
 }
 export default AppCard;
