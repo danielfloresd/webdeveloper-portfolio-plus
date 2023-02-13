c940e53d41ef0aa45e7fda6d81baa51b7a14c559
Daniel Flores D <daniel.flor3s.d@gmail.com>
2023-02-13 14:55:39 -0700

    Add form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { Form, Button, Card, Row, Col } from "react-bootstrap";
+import { Form, Button, Card, Row, Col, Alert } from "react-bootstrap";
+
+// Email regex
+const emailRegex = RegExp(
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+);
 
 function Contact() {
   const [validated, setValidated] = useState(false);
@@ -14,15 +19,56 @@ function Contact() {
     setValidated(true);
   };
 
+  const handleBlurName = () => {
+    const name = document.getElementById("formName").value;
+    if (name === "") {
+      setShow(true);
+      setError("Name is required");
+    } else {
+      setShow(false);
+    }
+  };
+
+  const handleBlurEmail = () => {
+    const name = document.getElementById("formEmail").value;
+    if (name === "") {
+      setShow(true);
+      setError("Email is required");
+    } else {
+      if (emailRegex.test(name)) {
+        setShow(false);
+      } else {
+        setShow(true);
+        setError("Email is not valid");
+      }
+    }
+  };
+
+  const handleBlurMessage = () => {
+
+    const name = document.getElementById("formMessage").value;
+  
+    if (name === "") {
+      setShow(true);
+      setError("Message is required");
+    } else {
+      setShow(false);
+    }
+  };
+
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState("");
+
   return (
     <Row>
-      <Col lg={8} >
+      <Col lg={8}>
         <Card>
           <Card.Body>
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
               <Form.Group controlId="formName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control
+                  onBlur={handleBlurName}
                   required
                   type="text"
                   placeholder="Enter your name"
@@ -30,16 +76,33 @@ function Contact() {
               </Form.Group>
               <Form.Group controlId="formEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control required type="email" placeholder="Enter email" />
+                <Form.Control
+                  onBlur={handleBlurEmail}
+                  required
+                  type="email"
+                  placeholder="Enter email"
+                />
               </Form.Group>
               <Form.Group controlId="formMessage">
                 <Form.Label>Message</Form.Label>
                 <Form.Control
+                  onBlur={handleBlurMessage}
+                  // onFocus={handleBlurMessage}
                   required
                   as="textarea"
                   placeholder="Enter message"
                 />
               </Form.Group>
+              {show ? (
+                <Alert
+                  variant="danger"
+                  // onClose={() => setShow(false)}
+                  dismissible
+                >
+                  {/* <Alert.Heading>Oh snap! You got an error!</Alert.Heading> */}
+                  <p>{error}</p>
+                </Alert>
+              ) : null}
               <Button type="submit">Submit</Button>
             </Form>
           </Card.Body>
